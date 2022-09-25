025aec83004b03468481349658e02a01d4533444
Mandreol <marcoandreola01@gmail.com>
2022-09-25 12:56:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import WeaderCard from "./component/WeaderCard";
+
+function App() {
+	const [coords, setCoords] = useState();
+	const [weather, setWeather] = useState();
+	let [scale, setScale] = useState("metric");
+	const handelScale = () => {
+		setScale(scale == "" ? (scale = "metric") : (scale = ""));
+	};
+
+	useEffect(() => {
+		const succes = (pos) => {
+			const coordsObj = {
+				lat: pos.coords.latitude,
+				lon: pos.coords.longitude,
+			};
+			setCoords(coordsObj);
+		};
+		navigator.geolocation.getCurrentPosition(succes);
+	}, []);
+
+	useEffect(() => {
+		if (coords) {
+			const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=41071a30ab316a7e45e850c195f53727&units=${scale}`;
+			axios
+				.get(URL)
+				.then((res) => setWeather(res.data))
+				.catch((err) => console.log(err));
+		}
+	}, [coords, scale]);
+	console.log(weather);
+
+	return (
+		<div className="App">
+			<video autoPlay loop muted>
+				<source src="./video/Nubes.mp4" type="video/mp4" />
+			</video>
+			<h1>Weather App</h1>
+			<WeaderCard weather={weather} scale={scale} handelScale={handelScale} />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/component/WeaderCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../styles/weader_card.css";
+
+const WeaderCard = ({ weather, scale, handelScale }) => {
+	const iconUrl = weather?.weather[0].icon;
+	return (
+		<div className="weaderCard">
+			<h2>
+				{weather?.name}, {weather?.sys.country}
+			</h2>
+			<img className="weaderCard__gif" src={`./icons/${iconUrl}.gif`} alt="" />
+			<ul className="weaderCard__secundaryInfo">
+				<li>
+					<img src="./icons/termico.png" alt="" />
+					<p>
+						Thermal sensation: {weather?.main.feels_like}
+						{scale == "metric" ? "°C" : "°K"}
+					</p>
+				</li>
+				<li>
+					<img src="./icons/humedad.png" alt="" />
+					<p>Humidity: {weather?.main.humidity}%</p>
+				</li>
+				<li>
+					<img src="./icons/temperaturas.png" alt="" />
+					<p>
+						T° max/min expected : {weather?.main.temp_max}
+						{scale == "metric" ? "°C" : "°K"} /{weather?.main.temp_min}
+						{scale == "metric" ? "°C" : "°K"}
+					</p>
+				</li>
+				<li>
+					<img src="./icons/visibilidad.png" alt="" />
+					<p>Visibility : {weather?.visibility / 1000}Km</p>
+				</li>
+				<li>
+					<img src="./icons/viento.png" alt="" />
+					Wind speed: {Math.round(weather?.wind.speed * 3.6)} Km/h
+				</li>
+			</ul>
+			<div className="weaderCard__principalInfo">
+				<h2>
+					Current temperature: {weather?.main.temp}
+					{scale == "metric" ? "°C" : "°K"}
+				</h2>
+				<h2>The weather right now: {weather?.weather[0].description}</h2>
+			</div>
+			<button className="button" onClick={handelScale}>
+				°C / °K
+			</button>
+		</div>
+	);
+};
+
+export default WeaderCard;
